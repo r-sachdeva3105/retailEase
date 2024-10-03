a3659d71d0852b31c7b4959343a91c537f58d920
Aditya Purohit <aditya.s.purohit@gmail.com>
2024-10-02 20:35:53 -0400

    transaction api and edit button

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BarcodeScanner } from "react-barcode-scanner";
 import "react-barcode-scanner/polyfill";
 import { BsUpcScan } from "react-icons/bs";
@@ -15,21 +15,80 @@ import {
   Label,
 } from "@headlessui/react";
 
-
-
 const Checkout = () => {
-
-
+  const [products, setProducts] = useState([]);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+  const [subTotal, setSubtotal] = useState(0.0);
+  const [tax, setTax] = useState(0.0);
+  const [total, setTotal] = useState(0.0);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        await fetch("http://localhost:8081/api-product/all-products")
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            setProducts(data);
+          });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchProducts();
+  }, []);
 
   const [newItem, setNewItem] = useState("");
-  const [transactionItems, setItems] = useState([], addItem);
+  const [transactionItems, setItems] = useState([], addItem); // transaction items
+  const [selectedIndex, setSelectedIndex] = useState(null);
+  const [currentEditQuantity, setCurrentEditQuantity] = useState(0);
 
+  function updateTotal(items, setSubtotal, setTotal) {
+    const newSubTotal = items.reduce(
+      (acc, item) => acc + parseFloat(item.productSellingPrice * item.quantity),
+      0
+    );
+    setSubtotal(newSubTotal);
 
+    const taxOnSubtotal = (0.13 * newSubTotal).toFixed(2);
+    setTax(parseFloat(taxOnSubtotal));
 
+    const totalWithTax = (newSubTotal + parseFloat(taxOnSubtotal)).toFixed(2);
+    setTotal(parseFloat(totalWithTax));
+  }
 
-  const handleRowClick = (row) => {
-    console.log('Row clicked:', row);
+  const toggleSelect = (index) => {
+    setSelectedIndex(selectedIndex === index ? null : index);
+    setCurrentEditQuantity(transactionItems[index].quantity);
+  };
+
+  const clearSelected = () => {
+    if (selectedIndex >= 0 && selectedIndex < transactionItems.length) {
+      let filteredTransactionList = transactionItems.filter(
+        (_, index) => index !== selectedIndex
+      );
+      setItems(filteredTransactionList);
+      updateTotal(filteredTransactionList, setSubtotal, setTotal);
+      setSelectedIndex();
+    }
+  };
+
+  const setTransactionItemQuantity = () => {
+    console.log(currentEditQuantity);
+    if (selectedIndex >= 0 && selectedIndex < transactionItems.length) {
+      let updatedTransactionList = transactionItems.map((item, index) => {
+        if (index === selectedIndex) {
+          return { ...item, quantity: currentEditQuantity };
+        }
+        return item;
+      });
+
+      setItems(updatedTransactionList);
+      updateTotal(updatedTransactionList, setSubtotal, setTotal);
+      setSelectedIndex();
+      setIsEditDialogOpen(false);
+    }
   };
 
   function handleInputChange(event) {
@@ -39,17 +98,36 @@ const Checkout = () => {
 
   function clearItems() {
     const clearedTransactionList = transactionItems.filter(() => false);
-    console.log("hello");
     setItems(clearedTransactionList);
+    updateTotal(clearedTransactionList, setSubtotal, setTotal);
   }
 
   function addItem() {
-    for (let i = 0; i < products.length; i++) {
-      if (products[i].productSKU === parseInt(newItem)) {
-        setItems([...transactionItems, products[i]])
-      }
+    const itemToAdd = products.find(
+      (product) => parseInt(product.productSKU) === parseInt(newItem)
+    );
+
+    const { productSKU, productName, productSellingPrice, discountID } =
+      itemToAdd;
+    const transactionWithQty = {
+      productSKU,
+      productName,
+      productSellingPrice,
+      discountID,
+      quantity,
+    };
+
+    if (transactionWithQty) {
+      const updatedItems = [...transactionItems, transactionWithQty];
+      setItems(updatedItems); // Update items first
+
+      // Update total using the updateTotal function
+      updateTotal(updatedItems, setSubtotal, setTotal);
+
+      // Reset newItem
+      setNewItem("");
     }
-    setNewItem("");
+    setIsDialogOpen(false);
   }
 
   const open = () => {
@@ -61,51 +139,17 @@ const Checkout = () => {
     setIsDialogOpen(false);
   };
 
-  // To verify productSKU
-  const isProductSKUFound = (skuToFind) => {
-    for (let i = 0; i < products.length; i++) {
-      if (products[i].productSKU === skuToFind) {
-        alert("yay")
-      }
-    }
-  }
+  const openEdit = () => {
+    setIsEditDialogOpen(true);
+  };
 
-  const products = [
-    {
-      "productSKU": 123456789,
-      "productName": "Sample Product",
-      "productCostPrice": 50.0,
-      "productSellingPrice": 75.0,
-      "productDescription": "This is a sample product description.",
-      "storageType": "Cool",
-      "isExpirable": true,
-      "expiryDate": "2025-12-31",
-      "discountID": 1,
-      "productCategoryName": "Electronics",
-      "productQuantity": 100,
-      "warehouseName": "Main Warehouse",
-      "productImage": "iVBORw0KGgoAA"
-    },
-    {
-      "productSKU": 987654321,
-      "productName": "Sample Product 2",
-      "productCostPrice": 50.0,
-      "productSellingPrice": 75.5,
-      "productDescription": "This is a sample product description.",
-      "storageType": "Cool",
-      "isExpirable": true,
-      "expiryDate": "2025-12-31",
-      "discountID": 1,
-      "productCategoryName": "Electronics",
-      "productQuantity": 100,
-      "warehouseName": "Main Warehouse",
-      "productImage": "iVBORw0KGgoAA"
-    }
-  ];
+  const closeEdit = () => {
+    setIsEditDialogOpen(false);
+  };
 
   const [isEnabled, setIsEnabled] = useState(false);
   const [barcodeData, setBarcodeData] = useState([]);
-  const [quantity, setQuantity] = useState(0);
+  const [quantity, setQuantity] = useState(1);
 
   const handleScan = (barcode) => {
     if (barcode) {
@@ -121,8 +165,61 @@ const Checkout = () => {
     console.log(barcodeData);
   };
 
-  const handleCheckout = () => {
+  const handleCheckout = async (e) => {
     setIsEnabled(false);
+    // if transaction list is not empty
+    if (transactionItems.length !== 0) {
+      const orderItem = {};
+
+      transactionItems.forEach((product) => {
+        const { productSKU, quantity } = product;
+
+        if (orderItem[productSKU]) {
+          orderItem[productSKU].orderQuantity += quantity;
+        } else {
+          orderItem[productSKU] = {
+            productSku: productSKU,
+            orderQuantity: quantity,
+          };
+        }
+      });
+
+      const orderItems = Object.values(orderItem);
+
+      const order = {
+        totalAmount: subTotal,
+        amountAfterTax: total,
+        employeeId: 5789,
+        orderItems,
+      };
+
+      try {
+        const response = await fetch(
+          "http://localhost:8081/api-orders/add-order",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(order),
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        if (response.ok) {
+          console.log("posted");
+          clearItems();
+        }
+
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
   };
 
   return (
@@ -163,29 +260,29 @@ const Checkout = () => {
                     </th>
                   </tr>
                 </thead>
-                {
-                  transactionItems.map((product, index) => (
-                    <tbody>
-                      <tr onClick={() => handleRowClick(product)}>
-                        <td className="px-4 py-3">
-                          {product.productName}
-                        </td>
-                        <td className="px-4 py-3">
-                          {product.productSellingPrice}
-                        </td>
-                        <td className="px-4 py-3">
-                          1
-                        </td>
-                        <td className="px-4 py-3">
-                          0.0
-                        </td>
-                        <td className="px-4 py-3">
-                          {product.productSellingPrice}
-                        </td>
-                      </tr>
-                    </tbody>
-                  ))
-                }
+                {transactionItems.map((product, index) => (
+                  <tbody>
+                    <tr
+                      key={index}
+                      onClick={() => toggleSelect(index)}
+                      style={{
+                        cursor: "pointer",
+                        backgroundColor:
+                          selectedIndex === index ? "#f0f0f0" : "transparent",
+                      }}
+                    >
+                      <td className="px-4 py-3">{product.productName}</td>
+                      <td className="px-4 py-3">
+                        {product.productSellingPrice}
+                      </td>
+                      <td className="px-4 py-3">{product.quantity}</td>
+                      <td className="px-4 py-3">0.0</td>
+                      <td className="px-4 py-3">
+                        {product.productSellingPrice * product.quantity}
+                      </td>
+                    </tr>
+                  </tbody>
+                ))}
               </table>
             </div>
             <div className="grid grid-cols-4 gap-x-2">
@@ -199,6 +296,7 @@ const Checkout = () => {
                 <button
                   type="button"
                   className="mt-2 inline-flex w-full h-16 justify-center rounded-md bg-red-700 px-3 py-5 text-md lg:text-lg font-semibold text-white shadow hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-700"
+                  onClick={clearSelected}
                 >
                   Delete
                 </button>
@@ -211,6 +309,7 @@ const Checkout = () => {
                 </button>
                 <button
                   type="button"
+                  onClick={openEdit}
                   className="mt-2 inline-flex w-full h-16 justify-center rounded-md bg-gray-600 px-3 py-5 text-md lg:text-lg font-semibold text-white shadow hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
                 >
                   Edit
@@ -354,7 +453,7 @@ const Checkout = () => {
                     type="number"
                     name="price"
                     id="price"
-                    class="block w-full rounded-md border-0 py-2 text-md lg:text-lg text-gray-900 font-bold ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:leading-6"
+                    className="block w-full rounded-md border-0 py-2 text-md lg:text-lg text-gray-900 font-bold ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:leading-6"
                     autoComplete="off"
                     value={quantity}
                     disabled
@@ -378,7 +477,9 @@ const Checkout = () => {
                   <dt className="text-sm font-medium text-gray-600">
                     Subtotal
                   </dt>
-                  <dd className="text-sm text-gray-900 font-medium">$0.00</dd>
+                  <dd className="text-sm text-gray-900 font-medium">
+                    ${subTotal}
+                  </dd>
                 </div>
                 <div className="flex justify-between">
                   <dt className="text-sm font-medium text-gray-600">
@@ -388,17 +489,35 @@ const Checkout = () => {
                 </div>
                 <div className="flex justify-between">
                   <dt className="text-sm font-medium text-gray-600">Tax</dt>
-                  <dd className="text-sm text-gray-900 font-medium">$0.00</dd>
+                  <dd className="text-sm text-gray-900 font-medium">
+                    HST @1.3% ${tax}
+                  </dd>
                 </div>
                 <div className="flex justify-between">
                   <dt className="text-md font-bold text-gray-800">Total</dt>
-                  <dd className="text-md font-bold text-gray-900">$0.00</dd>
+                  <dd className="text-md font-bold text-gray-900">${total}</dd>
                 </div>
               </dl>
               <button
                 type="button"
-                className="mt-2 inline-flex w-full h-16 justify-center rounded-md bg-sky-600 px-3 py-5 text-md lg:text-lg font-semibold text-white shadow hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
-                onClick={handleCheckout}
+                className={`mt-2 inline-flex w-full h-16 justify-center rounded-md 
+                  ${
+                    transactionItems.length === 0
+                      ? "bg-gray-400 text-gray-300 cursor-not-allowed"
+                      : "bg-sky-600 text-white"
+                  }
+                  px-3 py-5 text-md lg:text-lg font-semibold shadow 
+                  ${
+                    transactionItems.length === 0
+                      ? "hover:bg-gray-400"
+                      : "hover:bg-sky-500"
+                  }
+                  focus-visible:outline focus-visible:outline-2 
+                  focus-visible:outline-offset-2 focus-visible:outline-sky-600`}
+                onClick={
+                  transactionItems.length !== 0 ? handleCheckout : undefined
+                }
+                disabled={transactionItems.length === 0} //
               >
                 Proceed to Pay
               </button>
@@ -406,7 +525,6 @@ const Checkout = () => {
           </div>
         </div>
       </main>
-
 
       <Dialog
         as="div"
@@ -428,7 +546,10 @@ const Checkout = () => {
                   <Description className="text-sm/6 text-white/50">
                     Enter UPC of the item
                   </Description>
-                  <Input type="text" maxlength="9" pattern="[0-9]*" value={newItem} onChange={handleInputChange}
+                  <Input
+                    type="text"
+                    value={newItem}
+                    onChange={handleInputChange}
                     className={clsx(
                       "mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white",
                       "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
@@ -455,7 +576,100 @@ const Checkout = () => {
         </div>
       </Dialog>
 
-
+      <Dialog
+        as="div"
+        open={isEditDialogOpen}
+        onClose={closeEdit}
+        className="relative z-10 focus:outline-none"
+      >
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4">
+            <DialogPanel
+              transition
+              className="w-full max-w-md rounded-xl bg-gray-800 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+            >
+              <Field>
+                <form>
+                  <Label className="text-md font-medium text-white">
+                    Update Product
+                  </Label>
+                  <Description className="text-sm/6 text-white/50">
+                    Update quantity of product
+                  </Description>
+                  <div className="flex items-center justify-center my-3">
+                    <button
+                      className="flex justify-center items-center w-10 h-10 rounded-full text-white focus:outline-none bg-gray-600 hover:bg-gray-500"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setCurrentEditQuantity(currentEditQuantity - 1);
+                      }}
+                    >
+                      <svg
+                        className="w-6 h-6"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M20 12H4"
+                        ></path>
+                      </svg>
+                    </button>
+                    <Input
+                      type="number"
+                      className={clsx(
+                        "mx-3 w-20 rounded-lg border-none bg-white/5 py-1.5 px-3 text-lg font-bold text-white",
+                        "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25 text-center"
+                      )}
+                      value={currentEditQuantity}
+                    />
+                    <button
+                      className="flex justify-center items-center w-10 h-10 rounded-full text-white focus:outline-none bg-sky-600 hover:bg-sky-500"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setCurrentEditQuantity(currentEditQuantity + 1);
+                      }}
+                    >
+                      <svg
+                        className="w-6 h-6"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M12 6v12M6 12h12"
+                        ></path>
+                      </svg>
+                    </button>
+                  </div>
+                  <div className="mt-4 flex justify-end gap-4">
+                    <Button
+                      className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
+                      onClick={closeEdit}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      onClick={() => setTransactionItemQuantity()}
+                      className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
+                    >
+                      Save
+                    </Button>
+                  </div>
+                </form>
+              </Field>
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
     </div>
   );
 };
